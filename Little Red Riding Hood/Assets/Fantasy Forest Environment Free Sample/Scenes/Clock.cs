c1c9ff96c6fa8a3052e26f9842e095cc080f9823
OanaLolea <50479915+OanaLolea@users.noreply.github.com>
2020-03-30 20:23:35 +0300

    Little Red Riding Hood
    Minigame in which Little Red Riding Hood collects flowers for her grandmother before the timer expires.

---
+++ Little Red Riding Hood/Assets/Fantasy Forest Environment Free Sample/Scenes/Clock.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Clock : MonoBehaviour
+{
+    private int Minutes = 0;
+    private int Seconds = 0;
+
+    public TextMeshPro timer;
+    private float m_leftTime;
+    
+
+    private void Start()
+    {
+        //timer = GetComponent<TextMeshPro>();
+        this.Seconds = 30;
+        m_leftTime = GetInitialTime();
+    }
+
+    private void Update()
+    {
+        if (m_leftTime > 0f)
+        {
+            //  Update countdown clock
+            m_leftTime -= Time.deltaTime;
+            Minutes = GetLeftMinutes();
+            Seconds = GetLeftSeconds();
+
+            //  Show current clock
+            if (m_leftTime > 0f)
+            {
+                timer.text = "Time : " + Minutes + ":" + Seconds.ToString("00");
+            }
+            else
+            {
+                //  The countdown clock has finished
+                timer.text = "";
+            }
+        }
+    }
+
+    private float GetInitialTime()
+    {
+        return Minutes * 60f + Seconds;
+    }
+
+    private int GetLeftMinutes()
+    {
+        return Mathf.FloorToInt(m_leftTime / 60f);
+    }
+
+    private int GetLeftSeconds()
+    {
+        return Mathf.FloorToInt(m_leftTime % 60f);
+    }
+
+    public bool isOver()
+    {
+        return (Minutes == 0 && Seconds==0);
+    }
+}
---
+++ Little Red Riding Hood/Assets/Fantasy Forest Environment Free Sample/Scenes/scr.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class scr : MonoBehaviour
+    
+{
+    public Rigidbody girl;
+
+    public TextMeshPro counter;
+
+    Animator anim;
+
+    public Rigidbody flower01;
+    public Rigidbody flower02;
+    public Rigidbody flower03;
+    public Rigidbody flower04;
+    public Rigidbody flower05;
+    public Rigidbody flower06;
+
+    public Clock clock;
+
+    public GameObject pickFlowerSound;
+    public GameObject backgroundMusic;
+
+
+    public float movementSpeed { get; set; }
+    public float turnSpeed = 500f;
+
+    private int gatheredFlowers = 0;
+
+    private int totalFlowers = 6;
+
+    void print()
+    {
+        counter.text = "Flowers: "+ gatheredFlowers.ToString() + "/" + totalFlowers.ToString();
+    }
+
+    void Start()
+    {
+
+        backgroundMusic.GetComponent<AudioSource>().Play(0);
+        gatheredFlowers = 0;
+
+        anim = GetComponent<Animator>();
+
+        counter = counter.GetComponent<TextMeshPro>();
+
+        print();
+
+        movementSpeed = 5;
+
+        //clock = clock.GetComponent<Clock>();
+
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float move = Input.GetAxis("Vertical");
+        anim.SetFloat("Speed", move);
+
+        float angle = Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Mathf.Rad2Deg;
+
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            girl.transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+            anim.Play("run");
+
+        }
+        else
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            girl.transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
+            anim.Play("run");
+
+        }
+        else
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.Play("jump");
+            return;
+        }
+
+        
+        else
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //transform.Rotate(-Vector3.up * 25 * Time.deltaTime, Space.Self);
+            //girl.transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime);
+            //girl.transform.Rotate(0, -1, 0);
+           
+            girl.transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
+            
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //transform.Rotate(Vector3.up * 25 * Time.deltaTime, Space.Self);
+            //girl.transform.Rotate(Vector3.up * turnSpeed* Time.deltaTime);
+            //girl.transform.Rotate(0, 1, 0);
+
+            girl.transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
+
+        }
+        else
+            anim.Play("idle");
+
+
+        if (clock.isOver())
+            if (gatheredFlowers == totalFlowers)
+            {
+                // play a sound
+                counter.text = "Congratulations! You can visit granny";
+            }
+            else
+            {
+                // play another sound
+                counter.text = "You failed collecting flowers in time!";
+                //girl.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+                //girl.GetComponent<Rigidbody>().detectCollisions = false;
+                //StartCoroutine(waitToClose(5));
+
+                Application.Quit();
+            }
+
+    }
+
+    IEnumerator waitToClose(float duration)
+    {
+        
+        yield return new WaitForSeconds(duration);   //Wait
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.attachedRigidbody.name);
+        if (other.attachedRigidbody.name == "flower01" || other.attachedRigidbody.name == "flower02" || other.attachedRigidbody.name == "flower03" || other.attachedRigidbody.name == "flower04" || other.attachedRigidbody.name == "flower05" || other.attachedRigidbody.name == "flower06")
+            if(!clock.isOver()){
+                pickFlowerSound.GetComponent<AudioSource>().Play(0);
+                Destroy(other);
+                gatheredFlowers++;
+                print();
+                Destroy(other.gameObject);
+            }
+    }
+}
+   
