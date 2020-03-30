dc1d9acc7aa73b41c953a3d638dd403ddb484307
OanaLolea <50479915+OanaLolea@users.noreply.github.com>
2020-03-30 20:33:10 +0300

    .

---
+++ Ball/Assets/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+
+    
+{
+
+    public GameObject sphere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sphere = GameObject.Find("ball");
+        //sphere.GetComponent<Rigidbody>().AddForce(Vector3.forward * 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionStart(Collision col)
+    {
+        //sphere.GetComponent<Rigidbody>().AddForce(Vector3.back * 100);
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        
+        //sphere.GetComponent<Rigidbody>().AddForce(Vector3.back * 100);
+    }
+}
